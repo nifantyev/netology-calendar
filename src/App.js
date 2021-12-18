d5d9882acbe30bba1082291aa7c59123c66cc9c7
Евгений Нифантьев <nifantyev@gmail.com>
2021-12-18 11:53:12 +0300

    Поправил дату в App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Calendar from './components/Calendar';
 
 function App() {
-  const now = new Date(2021, 11, 31);
+  const now = new Date(2017, 2, 8);
   return <Calendar date={now} />;
 }
 
