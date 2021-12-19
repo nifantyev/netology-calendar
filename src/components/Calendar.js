f6b9189824da261cb95c69d40167cccc825c3175
Евгений Нифантьев <nifantyev@gmail.com>
2021-12-19 19:30:35 +0300

    В компоненте Calendar переписал работу с датами с помощью библиотеки date-fns

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,50 +1,67 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Week from './Week';
 import {
-  weekDays,
-  weekDaysShort,
-  months,
-  monthsGen,
-  DAYS_IN_WEEK,
-} from './constants';
+  format,
+  eachWeekOfInterval,
+  startOfMonth,
+  endOfMonth,
+  addDays,
+} from 'date-fns';
+import { ru } from 'date-fns/locale';
+import Week from './Week';
 
 const propTypes = {
   date: PropTypes.instanceOf(Date),
 };
 
-const getRussianWeekDay = (date) => {
-  return date.getDay() === 0 ? 6 : date.getDay() - 1;
+const capitalize = (s) => s[0].toUpperCase() + s.substr(1);
+
+const locale = ru;
+
+const DAYS_IN_WEEK = 7;
+
+const range = (n) => {
+  let arr = [];
+  for (let i = 0; i < n; i++) {
+    arr.push(i);
+  }
+  return arr;
 };
 
-const addDays = (date, days) => {
-  return new Date(date.getFullYear(), date.getMonth(), date.getDate() + days);
-};
+const weekDays = range(DAYS_IN_WEEK).map((i) =>
+  capitalize(
+    locale.localize.day((i + locale.options.weekStartsOn) % DAYS_IN_WEEK, {
+      width: 'wide',
+    })
+  )
+);
+
+const weekDaysShort = range(DAYS_IN_WEEK).map((i) =>
+  capitalize(
+    locale.localize.day((i + locale.options.weekStartsOn) % DAYS_IN_WEEK, {
+      width: 'short',
+    })
+  )
+);
 
 const Calendar = ({ date }) => {
-  // у нас неделя начинается с понедельника, а не с воскресенья
-  const weekDay = getRussianWeekDay(date);
-  const monthStart = new Date(date.getFullYear(), date.getMonth(), 1);
-  const monthEnd = new Date(date.getFullYear(), date.getMonth() + 1, 0);
-  const monthStartWeekDay = getRussianWeekDay(monthStart);
-  let weekStart = addDays(monthStart, -monthStartWeekDay);
-  const weeks = [];
-  while (weekStart <= monthEnd) {
-    const week = [];
-    for (let i = 0; i < DAYS_IN_WEEK; i++) {
-      week.push(addDays(weekStart, i));
-    }
-    weeks.push(week);
-    weekStart = addDays(weekStart, DAYS_IN_WEEK);
-  }
+  const weeks = eachWeekOfInterval(
+    { start: startOfMonth(date), end: endOfMonth(date) },
+    { locale: locale }
+  ).map((startOfWeek) => {
+    return range(DAYS_IN_WEEK).map((key) => addDays(startOfWeek, key));
+  });
+
   return (
     <div className="ui-datepicker">
       <div className="ui-datepicker-material-header">
-        <div className="ui-datepicker-material-day">{weekDays[weekDay]}</div>
+        <div className="ui-datepicker-material-day">
+          {capitalize(format(date, 'cccc', { locale: locale }))}
+        </div>
         <div className="ui-datepicker-material-date">
           <div className="ui-datepicker-material-day-num">{date.getDate()}</div>
           <div className="ui-datepicker-material-month">
-            {monthsGen[date.getMonth()]}
+            {format(date, 'MMMM', { locale: locale })}
           </div>
           <div className="ui-datepicker-material-year">
             {date.getFullYear()}
@@ -53,7 +70,9 @@ const Calendar = ({ date }) => {
       </div>
       <div className="ui-datepicker-header">
         <div className="ui-datepicker-title">
-          <span className="ui-datepicker-month">{months[date.getMonth()]}</span>
+          <span className="ui-datepicker-month">
+            {capitalize(format(date, 'LLLL', { locale: locale }))}
+          </span>
           &nbsp;
           <span className="ui-datepicker-year">{date.getFullYear()}</span>
         </div>
